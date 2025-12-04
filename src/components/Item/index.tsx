a79bc1c5af5bc1db43ed5274786d953a9bb0159e
nicolas4pitz <nicolas2004pitz@gmail.com>
2025-12-03 22:17:47 -0300

    Item

---
+++ src/components/Item/index.tsx
@@ -0,0 +1,13 @@
+
+
+const Item = ( {title} ) => {
+  return (
+    <>
+      <div className="outline-2 outline-emerald-500 p-4 rounded-2xl">
+        <h3 className="text-blue-950">{title}</h3>
+      </div>
+    </>
+  )
+}
+
+export default Item
