e6d98c098b6c3bc07581d63e39118166a34d0f8c
NicolasArthurDev <Nicolasarthur1806@gmail.com>
2025-12-03 21:28:51 -0300

    feat: adiciona shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import Home from './app/home'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const rootEl = document.getElementById('root') as HTMLElement
+
+createRoot(rootEl).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>
 )
