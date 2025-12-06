8b0d4aaf8fafa071a78dbdea96f469bd96754cf4
thnbi <renato.hnbi@gmail.com>
2025-12-06 15:55:48 -0300

    chore: adiciona teste para o componente Container

---
+++ tests/Container.test.tsx
@@ -0,0 +1,10 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import Container from '../src/components/Container';
+
+describe('componente container', () => {
+	it('deve renderizar o conteúdo corretamente', () => {
+		render(<Container>Hello World</Container>);
+		expect(screen.getByText('Hello World')).toBeInTheDocument();
+	});
+});
