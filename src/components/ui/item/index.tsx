d16201904060c708fc10f4a0f4f2bb7507d8ecc4
nicolas4pitz <nicolas2004pitz@gmail.com>
2025-12-05 22:07:26 -0300

    atualiza estilo item

--- src/components/ui/item/index.tsx
+++ src/components/ui/item/index.tsx
@@ -5,8 +5,8 @@ interface ItemProps {
 const Item = ({title} : ItemProps ) => {
   return (
     <>
-      <div className="flex justify-center items-center outline-2 outline-emerald-500 m-4 p-4 rounded-2xl h-20 w-28">
-        <h3 className="text-blue-950">{title}</h3>
+      <div className="flex justify-center items-center outline-2 outline-zinc-500 m-4 p-4 rounded-2xl h-20 w-28">
+        <h3 className="text-white">{title}</h3>
       </div>
     </>
   )
