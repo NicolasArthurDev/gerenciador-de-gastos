ed911bcb112800de1e52953368366f3a4ceedf69
nicolas4pitz <nicolas2004pitz@gmail.com>
2025-12-09 17:18:09 -0300

    fix: muda para const

--- src/components/ui/sidebar/index.tsx
+++ src/components/ui/sidebar/index.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 export default function Sidebar() {
 
-  let [activeTab, setActiveTab] = useState(0);
+  const [activeTab, setActiveTab] = useState(0);
 
   const links = [
     {
